eb95ba99a28e430abd9af4f00212927b999c2d6e
Sean Welgemoed <sean.welgemoed@ziflow.com>
2024-08-21 12:54:13 +0200

    setup gh-page

--- scripts/attack_area.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-signal player_attack
-
-func _ready():
-    connect("body_entered", _on_body_entered.bind(self))
-
-func _on_body_entered(body):
-    player_attack.emit(body)
--- scripts/pickup.gd
+++
@@ -1,8 +0,0 @@
-class_name Pickup
-extends Attribute
-
-signal picked
-
-func do_pickup(source: Box) -> void:
-	if !source.is_in_group("player"):
-		picked.emit()
